0183753dd45916a73b5e5925a014fad6f54d3a2c
Leah_Grace <leahgdg@hotmail.com>
2022-03-08 22:28:55 -0500

    "ApplesAndOranges Solved"

---
+++ src/com/LeahGrace/Practice/Algorithms/Implementation/ApplesAndOranges.java
@@ -0,0 +1,88 @@
+package com.LeahGrace.Practice.Algorithms.Implementation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class ApplesAndOranges {
+
+    class Result {
+
+        /*
+         * Complete the 'countApplesAndOranges' function below.
+         *
+         * The function accepts following parameters:
+         *  1. INTEGER s - - House area start
+         *  2. INTEGER t - - House area end
+         *  3. INTEGER a - - Apple Tree location
+         *  4. INTEGER b - - Orange Tree location
+         *  5. INTEGER_ARRAY apples - - Apple locations
+         *  6. INTEGER_ARRAY oranges - - Orange locations
+         */
+
+        public static void countApplesAndOranges(int houseStart, int houseEnd, int appleTree, int orangeTree, List<Integer> apples, List<Integer> oranges) {
+            // Write your code here
+
+            int appleCount = 0;
+            int orangeCount = 0;
+
+            for (int apple : apples){
+                if (appleTree + apple >= houseStart && appleTree + apple <= houseEnd)
+                    appleCount++;
+            }
+            for (int orange : oranges){
+                if (orangeTree + orange >= houseStart && orangeTree + orange <= houseEnd)
+                    orangeCount++;
+            }
+            System.out.println(appleCount);
+            System.out.println(orangeCount);
+
+        }
+
+    }
+
+    public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int s = Integer.parseInt(firstMultipleInput[0]);
+
+            int t = Integer.parseInt(firstMultipleInput[1]);
+
+            String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int a = Integer.parseInt(secondMultipleInput[0]);
+
+            int b = Integer.parseInt(secondMultipleInput[1]);
+
+            String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int m = Integer.parseInt(thirdMultipleInput[0]);
+
+            int n = Integer.parseInt(thirdMultipleInput[1]);
+
+            List<Integer> apples = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+            List<Integer> oranges = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+            Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+            bufferedReader.close();
+        }
+    }
+
+}
