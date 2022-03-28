7ee2df80cc686f7361242e1dae7ebab8fbbfcfe6
Leah Grace <Leahgdg@hotmail.com>
2022-03-28 14:09:01 -0400

    "Solved"

--- src/com/LeahGrace/CodeSignal/houseRobber.java
+++ src/com/LeahGrace/CodeSignal/houseRobber.java
@@ -4,34 +4,37 @@ import java.util.HashMap;
 
 public class houseRobber {
 
-
-    int solution(int[] nums) {
-        int sumOdd = 0;
-        int sumEven = 0;
-
-        HashMap map = new HashMap<>();
-        ListNode<Integer> head = null;
-
-        if (nums.length == 0) {
-            System.out.println("length is " + nums.length);
-            return 0;
-        }
-
-        if (nums.length == 1) {
-            System.out.println("length is " + nums.length);
-            return nums[0];
-        }
-
-        if (nums.length > 1) {
-            for (int i = 0; i < nums.length; i += 2) {
-                sumEven += nums[i];
+        int solution(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
             }
-            for (int j = 1; j < nums.length; j += 2) {
-                sumOdd += nums[j];
-            }
-            return (sumEven > sumOdd) ? sumEven : sumOdd;
-        }
 
-        return 0;
+            if (nums.length == 1) {
+                return nums[0];
+            }
+
+            if (nums.length == 2) {
+                return ((nums[0] >= nums[1]) ? nums[0] : nums[1]);
+            }
+
+            int[] cache = new int[nums.length];
+
+            cache[0] = nums[0];
+            cache[1] = Math.max(nums[1], cache[0]);
+
+            for (int i = 2; i < nums.length; i++) {
+                cache[i] = Math.max(nums[i] + cache[i - 2], cache[i - 1]);
+
+            }
+
+            int max = cache[0];
+            for (int i : cache) {
+                if (i >= max) {
+                    max = i;
+                }
+            }
+
+            return max;
+
+        }
     }
-}
